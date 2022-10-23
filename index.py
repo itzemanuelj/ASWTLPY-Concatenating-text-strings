0af1d7b624efecdc541ee8f3bb1b40889d178552
Emanuel Joness <itzemanuelj@gmail.com>
2022-10-23 01:15:31 -0400

    mvp commit for concatenating text strings

---
+++ index.py
@@ -0,0 +1,38 @@
+from typing import Concatenate
+# To combine two strings, you __________ them.
+# Concatenate
+
+# Strings have been assigned to the variables a and b. Concatenate them and assign the result to the variable c.
+a = 'hello '
+b = 'world'
+c = a + b
+print(c)
+
+c = a + b
+
+# Concatenate "Mr. " and "Brown". Assign the result to the variable person.
+person = "Mr. " + "Brown"
+
+# Concatenate first_string and second_string. Assign the result to the variable combo.
+first_string = 1
+second_string = 2
+combo = first_string + second_string
+
+# Concatenate "Hello, " and "world!". Assign the result to a variable. Make up the variable name. Don't forget the space at the end of the first string.
+greeting = "Hello, " + "World!"
+
+# Concatenate three strings: your first name, a space, and your last name. Assign the result to a variable. Make up the variable name.
+name = "justin" + "" + "jones"
+
+# Give yourself a fancy title, like Sir Mark or Lady Judith. Concatenate the title plus a space plus your first name. Assign the result to a variable. Make up the variable name.
+new_title = "lorde" + "" + "justin"
+
+# Give yourself a fancy title. But this time include the space in the first string. So you'll be concatenating the title and space as the first string, and your first name as the second string. Assign the result to a variable. Make up the variable name.
+
+new_name = "Lorde " + "Justin"
+
+# In a single statement, tell Python to display the concatenation of two variables. Make up the variable names.
+print(a + b)
+
+# code a single line, using the two variables plus a string between them, that displays Hello, World!
+print("Hello, " + "World!")
